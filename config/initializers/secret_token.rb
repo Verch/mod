a323866a27ac426e8551c62ef5aad4bb5f8746df
Administrator <user@dhcppc1.(none)>
2012-11-10 00:49:12 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Modernimport::Application.config.secret_token = 'f0d8c92b8883f3272e982179a521603bb91e5116540585229185fd75d7b53bca280c1acedd192e84f6c3d820239130dcdb04a1bac3176bdbd23ae614216a7684'
