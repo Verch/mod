0d1a8e08fe419c2e57ad596adca122b37fe0a430
Andrew Brainn <trufel_1324@tut.by>
2012-11-12 00:15:04 +0300

    users and users group fixing start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :company_name, :contact_name, :edit_data_flag, :email, :juridical_address, :last_in, :mailing_address, :name, :password_digest, :phone_number_0, :phone_number_1, :phone_number_2, :phone_number_3, :reg_confirm_admin, :skype, :unp, :work_time
+  belongs_to :user_groups
 end
--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,3 +1,4 @@
 class UserGroup < ActiveRecord::Base
   attr_accessible :admin_flag, :least_order, :name, :view_reserv_flag, :view_sklad_flag
+  has_many :users
 end
