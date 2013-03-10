d30d29c40be11ab60f1ff066c17efb1e07de9f02
Andrew Brainn <Bee.Brainn@gmail.com>
2013-03-11 02:18:40 +0300

    4

---
+++ add_link_in_product_type_show.rb
@@ -0,0 +1,110 @@
+product_types = ProductType.all
+product_types.each do |product_type|
+  if product_type.id == 1
+  	buffer = product_type.description
+    product_type.description = buffer+"<a href=\"http://www.modernimport.by/futbolki/1\">Мужские футболки в Минске</a> от МодернИмпорт.<br>"
+    product_type.save
+  else
+    if product_type.id == 2
+      buffer = product_type.description
+      product_type.description = buffer+"<a href=\"http://www.modernimport.by/futbolki/2\">Женские футболки в Минске</a> от МодернИмпорт.<br>"
+      product_type.save
+    else
+      if product_type.id == 3
+      	buffer = product_type.description
+      	product_type.description = buffer+"<a href=\"http://www.modernimport.by/futbolki/3\">Детские футболки в Минске</a> от МодернИмпорт.<br>"
+      	product_type.save
+  	  else
+  	  	if product_type.id == 4
+  	  	  buffer = product_type.description
+  	  	  product_type.description = buffer+"<a href=\"http://www.modernimport.by/futbolki/4\">Футболки оптом в Минске</a> от МодернИмпорт.<br>"
+  	  	  product_type.save
+  	  	else
+  	  	  if product_type.id == 5
+  	  	  	buffer = product_type.description
+  	  	  	product_type.description = buffer+"<a href=\"http://www.modernimport.by/futbolki/5\">Футболки в Минске</a> от МодернИмпорт.<br>"
+  	  	  	product_type.save
+  	  	  else
+  	  	  	if product_type.id == 6
+  	  	  	  buffer = product_type.description
+  	  	  	  product_type.description = buffer+"<a href=\"http://www.modernimport.by/futbolki/6\">Купить футболку</a> из хлопка в Минске.<br>" 
+  	  	  	  product_type.save
+  	  	    else
+  	  	      if product_type.id == 7
+  	  	      	buffer = product_type.description
+  	  	      	product_type.description = buffer+"<a href=\"http://www.modernimport.by/futbolki/7\">Качественные <a>футболки</a> в Минске.<br>"
+  	  	      	product_type.save
+  	  	      else
+  	  	      	if product_type.id == 8
+  	  	      	  buffer = product_type.description
+  	  	      	  product_type.description = buffer+"<a href=\"http://www.modernimport.by/futbolki/8\">Мужские, женские и детские футболки оптом в Минске</a>.<br>"
+  	  	      	  product_type.save
+  	  	        else
+  	  	          if product_type.id == 9
+  	  	      	    buffer = product_type.description
+  	  	      	    product_type.description = buffer+"<a href=\"http://www.modernimport.by/futbolki/9\">Мужские, женские и детские футболки оптом в Минске</a>.<br>"
+  	  	      	    product_type.save
+  	  	      	  else
+  	  	      	  	if product_type.id == 10
+  	  	      	  	  buffer = product_type.description
+  	  	      	  	  product_type.description = buffer+"<a href=\"http://www.modernimport.by/futbolki/10\">Мужские, женские и детские футболки оптом в Минске</a>.<br>"
+  	  	      	      product_type.save
+  	  	      	    else
+  	  	      	      if product_type.id == 11
+  	  	      	      	buffer = product_type.description
+  	  	      	      	product_type.description = buffer+"Покупайте <a href=\"http://www.modernimport.by/rubashki-polo/11\">рубашки поло в Минске</a> у Модерн Импорт!<br>"
+  	  	      	      	product_type.save
+  	  	      	      else
+  	  	      	      	if product_type.id == 12
+  	  	      	      	  buffer = product_type.description
+  	  	      	      	  product_type.description = buffer+"Покупайте <a href=\"http://www.modernimport.by/rubashki-polo/12\">рубашки поло в Минске</a> у Модерн Импорт!<br>"
+  	  	      	      	  product_type.save
+  	  	      	        else
+  	  	      	          if product_type.id == 13
+  	  	      	          	buffer = product_type.description
+  	  	      	          	product_type.description = buffer+"<a href=\"http://www.modernimport.by/tolstovki/13\">Покупайте <a>мужские толстовки</a> в Минске у Модерн Импорт!<br>"
+  	  	      	          	product_type.save
+  	  	      	          else
+  	  	      	            if product_type.id == 14
+  	  	      	          	  buffer = product_type.description
+  	  	      	          	  product_type.description = buffer+"<a href=\"http://www.modernimport.by/tolstovki/14\">Покупайте <a>мужские толстовки</a> в Минске у Модерн Импорт!<br>"
+  	  	      	          	  product_type.save
+  	  	      	            else
+  	  	      	              if product_type.id == 15
+  	  	      	              	buffer = product_type.description
+  	  	      	              	product_type.description = buffer+"Покупайте <a href=\"http://www.modernimport.by/beysbolki/15\">бейсболки</a> в Минске у Модерн Импорт!<br>"
+  	  	      	              	product_type.save
+  	  	      	              else
+  	  	      	                if product_type.id == 16
+  	  	      	                  buffer = product_type.description
+  	  	      	                  product_type.description = buffer+"Покупайте <a href=\"http://www.modernimport.by/beysbolki/16\">бейсболки</a> в Минске у Модерн Импорт!<br>"
+  	  	      	              	  product_type.save
+  	  	      	                else
+  	  	      	                  if product_type.id == 17
+  	  	      	              	    buffer = product_type.description
+  	  	      	              	    product_type.description = buffer+"Покупайте <a href=\"http://www.modernimport.by/beysbolki/17\">бейсболки</a> в Минске у Модерн Импорт!<br>"
+  	  	      	              	    product_type.save
+  	  	      	                  else
+  	  	      	                    if product_type.id == 18
+  	  	      	              	      buffer = product_type.description
+  	  	      	              	      product_type.description = buffer+"Покупайте <a href=\"http://www.modernimport.by/beysbolki/18\">бейсболки</a> в Минске у Модерн Импорт!<br>"
+  	  	      	              	      product_type.save
+  	  	      	                    end
+  	  	      	                  end
+  	  	      	                end
+  	  	      	              end
+  	  	      	            end
+  	  	      	          end
+  	  	      	        end
+  	  	      	      end
+  	  	      	    end
+  	  	      	  end
+  	  	      	end
+  	  	      end
+  	  	    end
+  	  	  end
+  	  	end
+  	  end
+  	end
+  end
+end
